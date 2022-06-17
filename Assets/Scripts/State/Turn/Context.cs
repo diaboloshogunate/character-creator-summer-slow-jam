64d4b5c2429b4a975519612562ce4302233f9a46
Robert Parker <diaboloshogunate@gmail.com>
2022-06-17 15:56:55 -0700

    Add enemy units to context

--- Assets/Scripts/State/Turn/Context.cs
+++ Assets/Scripts/State/Turn/Context.cs
@@ -7,13 +7,15 @@ namespace State.Turn
     {
         public TurnStateMachine StateMachine { get; private set; }
         public List<Entity> OurUnits { get; private set; }
+        public List<Entity> EnemyUnits { get; private set; }
         public bool IsPlayersTurn { get; private set; }
 
-        public Context(TurnStateMachine stateMachine, bool isPlayersTurn, List<Entity> ourUnits)
+        public Context(TurnStateMachine stateMachine, bool isPlayersTurn, List<Entity> ourUnits, List<Entity> enemyUnits)
         {
             StateMachine  = stateMachine;
             IsPlayersTurn = isPlayersTurn;
             OurUnits      = ourUnits;
+            EnemyUnits    = enemyUnits;
         }
     }
 }
